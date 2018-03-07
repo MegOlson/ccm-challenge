d1eed7be74b2b7125b4de0dc3d2b5e6600a62a7d
mo <meganannetteolson@yahoo.com>
2018-03-07 14:45:41 -0800

    Create application and re-style root page.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Christian Care Ministries</h1>
+        </header>
+        <p className="App-intro">
+          Search doctors or medical facilities near you.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
