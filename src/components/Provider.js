7ebb5321819da16b7d9a4c3bf7e52058c5dfc494
mo <meganannetteolson@yahoo.com>
2018-03-08 11:34:02 -0800

    Plug in collabsible element to api response.

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -3,24 +3,19 @@ import Collapsible from 'react-collapsible';
 
 const Provider = props => (
   <div className="provider-info">
-    {props.first &&
-      <p className="provider-key">First Name:
-        <span className="provider-heading"> {props.first}</span>
-      </p>}
-
-    {props.city && props.state &&
-      <p className="provider-key"> Location:
-        <span className="provider-heading"> {props.city}, {props.state}</span>
-      </p>}
 
     {props.error &&
       <p className="error-message">
         { props.error }
-      </p>}
+      </p>
+    }
 
-    <Collapsible trigger="Start here">
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>It can even be another Collapsible component. Check out the next section!</p>
+    <Collapsible trigger={props.first}>
+      {props.city && props.state &&
+        <p className="provider-key"> Location:
+          <span className="provider-heading"> {props.city}, {props.state}</span>
+        </p>
+      }
     </Collapsible>
 
   </div>
