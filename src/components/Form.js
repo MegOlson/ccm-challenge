2c473d4832164ca0c3898333386f036e3f673310
mo <meganannetteolson@yahoo.com>
2018-03-08 09:49:24 -0800

    DRY up form component.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 
-class Form extends React.Component {
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.props.getProviders}>
-          <input type="text" name="provider_type" placeholder="Provider Type"/>
-          <input type="number" name="zip" placeholder="Zip Code"/>
-          <button>Get Results</button>
-        </form>
-      </div>
-    );
-  }
-}
+const Form = props => (
+  <form onSubmit={props.getProviders}>
+    <input type="text" name="provider_type" placeholder="Provider Type"/>
+    <input type="number" name="zip" placeholder="Zip Code"/>
+    <button>Get Results</button>
+  </form>
+)
 
 export default Form;
