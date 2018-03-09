effbb418fd9197f21f2345b3b7654acca07c9f18
mo <meganannetteolson@yahoo.com>
2018-03-09 10:16:13 -0800

    Add text to titles for more descriptive instructions.

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 const Title = () => (
   <div className="title-text">
     <h1>Medical Finder</h1>
-    <h5>Search for pre-approved medical providers near you.</h5>
+    <h3>Christian Care Ministries</h3>
+    <h4>Find pre-approved medical providers near you.</h4>
+    <div className="line"/>
+    <p>Search by provider and location.</p>
+    <p>For a more specific search, fill out the additional search fields.</p>
   </div>
 )
 
